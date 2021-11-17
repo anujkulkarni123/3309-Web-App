3489b9f7b7ddadd513b6c2d1a3089895f0b63368
aadhieaswar <aadhieaswar@gmail.com>
2021-11-17 16:10:56 -0500

    added sql statement to create users table

--- assignment3/create_tables.sql
+++ assignment3/create_tables.sql
@@ -1,6 +1,19 @@
 -- CREATE TABLE STATEMENTS --
 
+-- Users Table --
+CREATE TABLE Users (
+    UserID INT AUTO_INCREMENT
+    ,Username VARCHAR(50)
+    ,Password VARCHAR(50)
+    ,CreditCardNo VARCHAR(16)
+    ,Address VARCHAR(100)
+    ,Rating INT CHECK(Rating IN (1, 2, 3, 4, 5))
+    ,PRIMARY KEY (UserID)
+)
+
 -- Tool Table --
-CREATE TABLE Tools (
-    ToolID INT
+CREATE TABLE AvailableTools (
+    ToolID INT AUTO_INCREMENT
+    ,UserID INT
+    ,PRIMARY KEY (ToolID)
 );

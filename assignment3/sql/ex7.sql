43172a997ab0c7c583a14be4476c877bd2a2a9af
Andrew Schaus <andrewschaus3@gmail.com>
2021-11-22 23:48:29 -0500

    views

--- assignment3/sql/ex7.sql
+++ assignment3/sql/ex7.sql
@@ -1,3 +1,4 @@
+-- View of rows which show users and the tools they own --
 CREATE VIEW searchTools(toolName, toolType, Price, Username, Rating)
 AS SELECT 
 	t.toolName, 
@@ -9,5 +10,27 @@ FROM
 	Users u, Tools t
 WHERE
 	u.UserID = t.UserID;
-
 	
+  
+-- View of rows which show the most popular users based on recent transactions --
+CREATE VIEW popularTransactions(Username, Address, Rating, TransDone)
+AS SELECT
+    TOP 20
+    Username
+    ,Address
+    ,Rating
+    ,TransDone
+FROM
+    Users u
+    JOIN UserTransactions ut
+        ON (u.UserID = ut.SellerID)
+ORDER BY
+    TransactionDate
+    ,TransDone
+    
+  
+CREATE VIEW(Users)
+AS SELECT
+    *
+FROM
+    Users;

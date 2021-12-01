018b9c329bed47e587ee64b1a98f39852f8d67c1
Andrew Schaus <andrewschaus3@gmail.com>
2021-12-01 17:27:34 -0500

    Table

--- assignment4/WebApp/src/Table/Table.js
+++ assignment4/WebApp/src/Table/Table.js
@@ -1,5 +1,6 @@
 import './table.css'
 import React, { useState, useEffect, useContext } from 'react';
+import ReactPaginate from 'react-paginate';
 
 function Table()
 {
@@ -8,6 +9,32 @@ function Table()
 
         //initializing use state
         const [ search, setSearch ] = useState(' ')
+
+        const [pagination, setPagination] = useState({
+            data: new Array(1000).fill().map((value, index) => (({
+                id: tools,
+                title: tools.name,
+                body: tools.price
+              }))),
+            offset: 0,
+            numberPerPage: 10,
+            pageCount: 0,
+            currentData: []
+          });
+
+          useEffect(() => {
+            setPagination((prevState) => ({
+              ...prevState,
+              pageCount: prevState.data.length / prevState.numberPerPage,
+              currentData: prevState.data.slice(pagination.offset, pagination.offset + pagination.numberPerPage)
+            }))
+          }, [pagination.numberPerPage, pagination.offset])
+          
+          const handlePageClick = event => {
+            const selected = event.selected;
+            const offset = selected * pagination.numberPerPage
+            setPagination({ ...pagination, offset })
+          }
 
             //handling change in input
         const handleChange = e =>   {
@@ -19,13 +46,28 @@ function Table()
             tools.name.toLowerCase().includes(search.toLowerCase())
         )
 
-        return(
-            <div> 
-                {filteredTools.map(tools =>  {
-                    return(
-                        <Tool />
-                    );
-                })}
+        return (
+            <div>
+              {pagination.currentData && pagination.currentData.map(((item, index) => (
+                <div className="post">
+                  <h3>{`${item.title} - ${item.id}`}</h3>
+                  <p>{item.body}</p>
+                </div>
+              )))
+              }
+              <ReactPaginate
+                previousLabel={'previous'}
+                nextLabel={'next'}
+                breakLabel={'...'}
+                pageCount={pagination.pageCount}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={handlePageClick}
+                containerClassName={'pagination'}
+                activeClassName={'active'}
+              />
             </div>
-        );
+          );
+
+    export default Table;
 }

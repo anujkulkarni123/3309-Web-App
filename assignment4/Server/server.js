a1f342c2d98032c142be2456cdcf224073629f83
aadhieaswar <aadhieaswar@gmail.com>
2021-12-01 16:16:17 -0500

    connected to db

--- assignment4/Server/server.js
+++ assignment4/Server/server.js
@@ -5,8 +5,24 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
+// import db connection
+const createConnection = require('./db/connection');
+
 // constant variables
 const port = 5000;
+
+//
+router.get('/check-conn', (req, res) => {
+  const conn = createConnection();
+
+  conn.query('SELECT 1 + 1 AS solution', function (err) {
+    if (err) throw err
+
+    res.send({ status: 'pass', message: 'db server running successfully' });
+  });
+
+  conn.end();
+});
 
 // enable app to use the router
 app.use('/', router);

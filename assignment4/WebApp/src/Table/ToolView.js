970e595141c4721f47d0680dcbb0488b28b71141
aadhieaswar <aadhieaswar@gmail.com>
2021-12-04 14:09:08 -0500

    added content to expand in tools

--- assignment4/WebApp/src/Table/ToolView.js
+++ assignment4/WebApp/src/Table/ToolView.js
@@ -3,12 +3,27 @@ import { FaChevronCircleDown } from 'react-icons/fa';
 import Expand from 'react-expand-animated';
 import './Toolview.css';
 import $ from "jquery";
+import axios from 'axios';
 
 const ToolView =  ({ ID, Type, Name, UserID, CompanyID, Price, ForSale, ForRent}) => {
 
     const [clicked, setClicked] = useState(false);
-    
-    
+    const [toolSpecifics, setToolSpecifics] = useState('');
+
+    const displayToolData = (id) => {
+        setClicked(!clicked);
+
+        axios.get(`http://localhost:5000/tools/${id}`)
+            .then(({data}) => {
+                if (data) {
+                    setToolSpecifics(data);
+                }
+            })
+            .catch((err) => {
+                setToolSpecifics(err.message);
+            });
+    }
+
     return (
         <div className="tool-container">
             <div className="tool-div">
@@ -16,13 +31,16 @@ const ToolView =  ({ ID, Type, Name, UserID, CompanyID, Price, ForSale, ForRent}
                 <label className="name">{Name}</label>
                 <label className="price">{Price}</label>
                 <label className="type">{Type}</label>
-                <FaChevronCircleDown className="icon" onClick={() => setClicked(!clicked)}/>
+                <FaChevronCircleDown className="icon" onClick={() => displayToolData(ID)}/>
             </div>
 
             <Expand className="expand" open={clicked}>
-                <div className="expandDiv" style={{height: '400px', color: 'red' }}></div>
+                <div className="expandDiv" style={{height: '400px', color: 'red' }}>
+                    <div>{toolSpecifics.Username}</div>
+                    <div>{toolSpecifics.Address}</div>
+                </div>
             </Expand>
-           
+
         </div>
     );
 }

974a58fd59453e6e8ec8b28dee0ac6f93a8959f3
Jarrod Hasnain <jhasnain@uwo.ca>
2021-12-04 13:55:43 -0500

    Fixed conflict

--- assignment4/WebApp/src/index.js
+++ assignment4/WebApp/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import App from './App';
 import Login from './Login';
+import User from './User';
 import InsertTools from './insertTools/InsertTool';
 import TableView from './Table/TableView';
 
@@ -14,6 +15,7 @@ ReactDOM.render(
         <Route path="/App" element={<App />}></Route>
         <Route path="/" element={<Login />}></Route>
         <Route path='/InsertTool' element={<InsertTools />}></Route>
+        <Route path="/User" element={<User />}></Route>
       </Routes>
     </Router>
   </React.StrictMode>,

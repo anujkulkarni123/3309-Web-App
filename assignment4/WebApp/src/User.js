f8aad983331b2e5c3c9a01ae4354fdb44737764c
Jarrod Hasnain <jhasnain@uwo.ca>
2021-12-04 14:29:07 -0500

    Added js-cookie

--- assignment4/WebApp/src/User.js
+++ assignment4/WebApp/src/User.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import './User.css';
+import Cookies from 'js-cookie';
 
 class User extends Component {
+    state = {User: Cookies.get('user')};
     render() {
+        console.log(this.state.User);
         return(
             <div className="profile-page">
                 <Header />
                 <div className="profile-body">
                     <h1>
-                        Welcome, 
+                        Welcome, {this.state.User}
                     </h1>
                 </div>
             </div>

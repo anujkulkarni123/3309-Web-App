29151bc0105332f56cfb089f223f4c5214a5ce78
Menash.F <menashfareed@gmail.com>
2021-12-04 15:19:30 -0800

    k

--- assignment4/WebApp/src/index.js
+++ assignment4/WebApp/src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import App from './App';
 import Login from './Login';
 import User from './User';
-// import InsertTools from './insertTools/InsertTool';
+import InsertTools from './insertTools/InsertTool';
 import TableView from './Table/TableView';
 
 ReactDOM.render(
@@ -13,14 +13,8 @@ ReactDOM.render(
     <Router>
       <Routes>
         <Route path="/App" element={<App />}></Route>
-<<<<<<< HEAD
         <Route path="/" element={<InsertTools />}></Route>
         <Route path="/insertTool" element={<InsertTools />}></Route>
-=======
-        <Route path="/" element={<Login />}></Route>
-        {/*<Route path='/InsertTool' element={<InsertTools />}></Route>*/}
-        <Route path="/User" element={<User />}></Route>
->>>>>>> 287ca5048ed74126880cdc8d17ecabc625d04512
       </Routes>
     </Router>
   </React.StrictMode>,

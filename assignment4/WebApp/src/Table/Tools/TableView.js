bbfbdabe6d433f307caa2e2b4e031de94de113ef
aadhieaswar <aadhieaswar@gmail.com>
2021-12-05 14:51:03 -0500

    added pagination

--- assignment4/WebApp/src/Table/Tools/TableView.js
+++ assignment4/WebApp/src/Table/Tools/TableView.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, Component } from 'react'
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch, FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import $ from "jquery";
 import './tableview.css'
 import axios from 'axios';
@@ -13,6 +13,7 @@ import {
     Link,
     useNavigate
   } from "react-router-dom";
+import Paginate from 'react-paginate';
 
 
 $(function()    {
@@ -24,33 +25,59 @@ $(function()    {
 const comboboxData = [ "ToolName", "Price", "ToolType" ];
 
 class TableView extends Component {
-    
+
     state = {
         column: "ToolID",
         tools: [],
         results: [],
-        displayResults: false
+        displayResults: false,
+        perPage: 10,
+        currentPage: 0,
+        offset: 0,
+        search: ''
     }
 
     componentDidMount() {
         this.getTools();
     }
 
-
-    
-
     getTools = () => {
-        const { column } = this.state;
+        const { column, results } = this.state;
+        if (results) {
+            const slice = results.slice(this.state.offset, this.state.offset + this.state.perPage);
+            this.setState({
+                results: slice,
+                pageCount: Math.ceil(results.length / this.state.perPage)
+            })
+        }
+
         axios.get(`http://localhost:5000/tools/order/${column}`)
             .then(({data}) => {
                 console.log(data.data);
-                this.setState({tools: data.data, displayResults: false});
+                const slice = data.data.slice(this.state.offset, this.state.offset + this.state.perPage);
+                this.setState({
+                    tools: slice,
+                    displayResults: false,
+                    pageCount: Math.ceil(data.data.length / this.state.perPage)
+                });
             })
             .catch((err) => {
                 console.error(err);
             });
     }
-    
+
+    handlePageClick = (e) => {
+        const selectedPage = e.selected;
+        const offset = selectedPage * this.state.perPage;
+
+        this.setState({
+            currentPage: selectedPage,
+            offset: offset
+        }, () => {
+            this.getTools();
+        });
+    }
+
     handleFilterChange(e)    {
         console.log(e);
         this.setState({column: e});
@@ -59,8 +86,9 @@ class TableView extends Component {
 
     renderTool = ({ ToolID, ToolName, Price, ToolType, UserID }) => <ToolView key={ToolID} ID={ToolID} Name={ToolName} Price={Price} Type={ToolType} UserID={UserID}></ToolView>
 
-    handleToolSearch = (e) => {
-        this.filterTools(e.target.value);
+    handleToolSearch = () => {
+        this.setState(this.state.search)
+        this.filterTools(this.state.search);
     }
 
     filterTools = (search) => {
@@ -72,6 +100,9 @@ class TableView extends Component {
         const { tools } = this.state;
 
         const results = tools.filter((tool) => {
+            this.setState({
+                currentPage: 0
+            });
             return tool.ToolName.toLowerCase().includes(search.toLowerCase());
         });
 
@@ -80,7 +111,7 @@ class TableView extends Component {
 
     render()    {
         const { tools, results, displayResults } = this.state;
-        return (        
+        return (
             <div>
                 <div className="filter-div">
                     <div className="search-div">
@@ -89,12 +120,12 @@ class TableView extends Component {
                         </label>
                         <form>
                             <input className="input" class="input" type="search" placeholder="search" onChange={this.handleToolSearch}></input>
-                        </form> 
+                        </form>
                     </div>
 
                     <div className="sort-div">
                         <div classname="sort-label">
-                            <label >Sort By:</label> 
+                            <label >Sort By:</label>
                         </div>
                         <ComboBox id="combo-box" class="combo-box" options={comboboxData} onOptionsChange={this.handleFilterChange.bind(this)} enableAutocomplete/>
                     </div>
@@ -106,6 +137,18 @@ class TableView extends Component {
 
                 <div>
                     {!displayResults ? tools.map(this.renderTool) : results.map(this.renderTool)}
+                    <Paginate
+                        previousLabel={<FaArrowLeft style={{ color: 'var(--header)' }}/>}
+                        nextLabel={<FaArrowRight style={{ color: 'var(--header)' }} />}
+                        breakLabel={"..."}
+                        pageCount={this.state.pageCount}
+                        marginPagesDisplayed={2}
+                        pageRangeDisplayed={5}
+                        onPageChange={this.handlePageClick}
+                        containerClassName={"pagination"}
+                        subContainerClassName={"pages pagination"}
+                        activeClassName={"pg-active"}
+                    />
                 </div>
             </div>
         );

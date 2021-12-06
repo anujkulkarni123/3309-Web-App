827a737f232aa10ee4f1d79cc4c4dfcacfa1c7af
aadhieaswar <aadhieaswar@gmail.com>
2021-12-05 19:30:57 -0500

    i dont know

---
+++ assignment4/WebApp/src/Tools/Fav.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+// component to like/unlike tools
+export default class Fav extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
